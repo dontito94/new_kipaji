e35a33749db1ecbc2385eca08b40de1892c2f47e
francistito <francistito07@gmail.com>
2020-11-05 00:11:25 +0300

    side bar

--- frontend/src/components/home/SideMenu.vue
+++ frontend/src/components/home/SideMenu.vue
@@ -7,6 +7,13 @@
                 <span class="navbar-toggler-icon"></span>
             </a>
 
+                <a class="navbar-brand">KIPAJI</a>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+
+
         </nav>
         <div id="wrapper" class="toggled">
             <!-- Sidebar -->
